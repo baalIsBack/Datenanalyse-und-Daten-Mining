e1e0aafe6ee1cbe99cafb05b2e68fc6697ba2c6e
baalIsBack <mathiasschulte@aol.com>
2021-11-13 19:55:13 +0100

    little changes

--- main.py
+++ main.py
@@ -28,38 +28,46 @@ def Net():
     layers.append(nn.LogSoftmax(1))
     return nn.Sequential(*layers)
 
-def train(model, device, train_loader, optimizer):
+def train(model, device, loader, optimizer):
     model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
+    for batch_idx, (data, target) in enumerate(loader):
+        data = data.to(device)
+        target = target.to(device)
         output = model(data)
         loss = F.nll_loss(output, target)
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         if batch_idx % 10 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                0, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
+            print('{:.0f}%({}/{}) \tLoss: {:.8f}'.format(
+                100. * batch_idx / len(loader),
+                batch_idx * len(data),
+                len(loader.dataset),
+                loss.item()
+            ))
 
 
-def test(model, device, test_loader):
+def test(model, device, loader):
     model.eval()
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
+        for data, target in loader:
+            data = data.to(device)
+            target = target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            test_loss += F.nll_loss(output, target, reduction='sum').item()
+            prediction = output.argmax(dim=1, keepdim=True)#index of max log-probability
+            correct += prediction.eq(target.view_as(prediction)).sum().item()
 
-    test_loss /= len(test_loader.dataset)
+    test_loss /= len(loader.dataset)
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+    print('Average_loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss,
+        correct,
+        len(loader.dataset),
+        100. * correct / len(loader.dataset)
+    ))
 
 
 def main():
@@ -78,8 +86,7 @@ def main():
     model = Net().to(device)
     print(model)
     print()
-    print()
-    optimizer = optim.Adadelta(model.parameters(), lr = 1.0)
+    optimizer = optim.Adadelta(model.parameters(), lr = 1.0)#TODO change learning algorithm to backprop maybe?
 
     try:
         model.load_state_dict(torch.load(MODEL_SAVE_LOCATION))
@@ -90,11 +97,12 @@ def main():
     scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
 
     for epoch in range(1, 10):
+        print("Entering epoch " + str(epoch))
         train(model, device, train_loader, optimizer)
         test(model, device, test_loader)
         scheduler.step()
         torch.save(model.state_dict(), MODEL_SAVE_LOCATION)
-        print("Saving was successfull!")
+        print("Saved successfully!")
         print()
 
 if __name__ == '__main__':
