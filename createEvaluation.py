fa7aae7aec1713261a91fb0ecbd48da7c6444add
cJulian <julian.haubold@tu-dortmund.de>
2022-01-19 04:09:10 +0100

    Erstellung der algorithmen zum befüllen der Sheets

--- createEvaluation.py
+++ createEvaluation.py
@@ -6,10 +6,13 @@ numberOfClients = 10
 path = 'Evaluation/FLStandard/'
 
 def main(): #Erstellung von Workbook Versionen
-    for i in range (1,3):
+    if input("Pls confirm the override / creation of the Evaluations Sheets (y) : ").lower() == "y":
         addVersionWorkbook('Version1')
         addVersionWorkbook('Version2')
         addVersionWorkbook('Version3')
+        print("Sheets are created / replaced")
+    else:
+        print("Program aborted")
 
 def addVersionWorkbook(name): #Erstellung von einem Workbook
     if (numberOfClients > 20):
@@ -22,8 +25,8 @@ def addVersionWorkbook(name): #Erstellung von einem Workbook
     addEvaluationSheet('Global Accuracy', wb)
     for clientid1 in range(numberOfClients):
         for clientid2 in range(numberOfClients):
-            addEvaluationSheet('CM ' + str(clientid1) + str(clientid2), wb)
-
+            addEvaluationSheet('CM ' + str(clientid2) + str(clientid1), wb)
+    wb.active = wb['Global Accuracy']
     wb.save(path+name+'.xlsx')
 
 def addEvaluationSheet(name, wb): #Erstellung von Worksheets
@@ -35,6 +38,12 @@ def addEvaluationSheet(name, wb): #Erstellung von Worksheets
     for row in ws.iter_rows(1, 1, 2, 16):
         for cell in row:
             cell.value = "Run" + str(pointer)
+            pointer += 1
+    # Erstellung der Durchschnittswerte
+    pointer = 2
+    for row in ws.iter_rows(2, 22, 1, 1):
+        for cell in row:
+            cell.value = "=Average(B" + str(pointer) + ":P" + str(pointer) + ")"
             pointer += 1
 
 if __name__ == '__main__':
